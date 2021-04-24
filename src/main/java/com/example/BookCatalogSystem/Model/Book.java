100c88eb6f49e94e3e35e2d071a35c2ba93c7182
kshitij varshney <kshitijvarshney92@gmail.com>
2021-04-24 15:09:55 +0530

    create Book.java

---
+++ src/main/java/com/example/BookCatalogSystem/Model/Book.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 24-Apr-21
+ *   Time: 3:09 PM
+ *   File: Book.java
+ */
+
+package com.example.BookCatalogSystem.Model;
+
+public class Book {
+}
+
