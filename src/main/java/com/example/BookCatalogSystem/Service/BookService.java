6ba9e2ab045b27afc76c5ae6b6eb64ece29494e9
kshitij varshney <kshitijvarshney92@gmail.com>
2021-04-24 15:19:26 +0530

    create BookService.java

---
+++ src/main/java/com/example/BookCatalogSystem/Service/BookService.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 24-Apr-21
+ *   Time: 3:19 PM
+ *   File: BookService.java
+ */
+
+package com.example.BookCatalogSystem.Service;
+
+public class BookService {
+}
+
