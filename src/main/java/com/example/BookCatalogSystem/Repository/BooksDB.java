d1287392d876ad5e8d89c939cfec1a4e45f490f1
kshitij varshney <kshitijvarshney92@gmail.com>
2021-04-24 15:23:46 +0530

    create BooksDB.java

---
+++ src/main/java/com/example/BookCatalogSystem/Repository/BooksDB.java
@@ -0,0 +1,12 @@
+/*   Created by IntelliJ IDEA.
+ *   Author: Kshitij Varshney (kshitijvarshne1)
+ *   Date: 24-Apr-21
+ *   Time: 3:23 PM
+ *   File: BooksDB.java
+ */
+
+package com.example.BookCatalogSystem.Repository;
+
+public class BooksDB {
+}
+
