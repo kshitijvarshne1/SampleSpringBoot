94688e81c96857c2a00e11c01a58dcff72589512
kshitij varshney <kshitijvarshney92@gmail.com>
2021-04-24 15:01:42 +0530

    Initial commit

---
+++ src/main/java/com/example/BookCatalogSystem/BookCatalogSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.BookCatalogSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookCatalogSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookCatalogSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BookCatalogSystem/BookCatalogSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BookCatalogSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookCatalogSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
