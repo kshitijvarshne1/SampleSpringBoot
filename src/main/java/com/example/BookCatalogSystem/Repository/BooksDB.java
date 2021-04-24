fa92e7c2acf5b9d8e11a2ad353c7fa63dd9bd585
kshitij varshney <kshitijvarshney92@gmail.com>
2021-04-24 15:27:48 +0530

    add insertBook method in BookDB.java

--- src/main/java/com/example/BookCatalogSystem/Repository/BooksDB.java
+++ src/main/java/com/example/BookCatalogSystem/Repository/BooksDB.java
@@ -7,6 +7,24 @@
 
 package com.example.BookCatalogSystem.Repository;
 
+import com.example.BookCatalogSystem.Model.Book;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+
+@Repository
 public class BooksDB {
+    ArrayList<Book> booksList= new ArrayList<>();
+
+    public String insertBook(Book book){
+        for (Book book1 : booksList) {
+            if(book1.getId()==book.getId()){
+                return "Id exist already";
+            }
+        }
+        booksList.add(book);
+        System.out.println(book);
+        return "Book added successfully";
+    }
 }
 
